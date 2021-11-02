950e72900c0e910e1c55db3fa46ca4bffaf4f22e
cventastic <sven@sven-holter.de>
2021-11-02 19:34:09 +0100

    third commit

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -e
+BUNDLR=/opt/bundler-node/
 cd /opt
-git clone https://github.com/Bundlr-Network/bundler-node.git
+if [ ! -d $BUNDLR ]; then
+  git clone https://github.com/Bundlr-Network/bundler-node.git
+fi
 cd bundler-node
 cp .env.example .env
-sed -i 's/^DATABASE_HOST.*/DATABASE_HOST=postgres/' /opt/bundler-node/.env
-sed -i 's/^DATABASE_USER.*/DATABASE_USER=bundlr/' /opt/bundler-node/.env
-sed -i 's/^DATABASE_PASSWORD.*/DATABASE_PASSWORD=changeme/' /opt/bundler-node/.env
-sed -i 's/^DATABASE_NAME.*/DATABASE_NAME=bundlr/' /opt/bundler-node/.env
-sed -i 's/^REDIS_HOST.*/REDIS_HOST=redis/' /opt/bundler-node/.env
-sed -i 's/^REDIS_PORT.*/REDIS_PORT=6379/' /opt/bundler-node/.env
+sed -i 's/^DATABASE_HOST.*/DATABASE_HOST=postgres/' $BUNDLR/.env
+sed -i 's/^DATABASE_USER.*/DATABASE_USER=bundlr/' $BUNDLR/.env
+sed -i 's/^DATABASE_PASSWORD.*/DATABASE_PASSWORD=changeme/' $BUNDLR/.env
+sed -i 's/^DATABASE_NAME.*/DATABASE_NAME=bundlr/' $BUNDLR/.env
+sed -i 's/^REDIS_HOST.*/REDIS_HOST=redis/' $BUNDLR/.env
+sed -i 's/^REDIS_PORT.*/REDIS_PORT=6379/' $BUNDLR/.env
 
 yarn
-chmod +x scripts/reset_all.sh
-scripts/reset_all.sh
+chmod +x $BUNDLR/scripts/reset_all.sh
+$BUNDLR/scripts/reset_all.sh
 
 yarn prod:build
 yarn prod:start
