ed039fad3cd880cca8d1d7d85bbf13c31b895f9e
cventastic <sven@sven-holter.de>
2021-11-02 18:45:32 +0100

    second commit

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 set -e
-sleep infinity
-#cd /opt
-#git clone https://github.com/Bundlr-Network/bundler-node.git
-#cd bundler-node
-#cp .env.example .env
-#sed -i 's/^DATABASE_HOST.*/DATABASE_HOST=postgres/' /opt/bundler-node/.env
-#sed -i 's/^DATABASE_USER.*/DATABASE_USER=bundlr/' /opt/bundler-node/.env
-#sed -i 's/^DATABASE_PASSWORD.*/DATABASE_PASSWORD=changeme/' /opt/bundler-node/.env
-#sed -i 's/^DATABASE_NAME.*/DATABASE_NAME=bundlr/' /opt/bundler-node/.env
-#sed -i 's/^REDIS_HOST.*/REDIS_HOST=redis/' /opt/bundler-node/.env
-#sed -i 's/^REDIS_PORT.*/REDIS_PORT=6379/' /opt/bundler-node/.env
-#
-#yarn
-#chmod +x scripts/reset_all.sh
-#scripts/reset_all.sh
-#
-#yarn prod:build
-#yarn prod:start
+cd /opt
+git clone https://github.com/Bundlr-Network/bundler-node.git
+cd bundler-node
+cp .env.example .env
+sed -i 's/^DATABASE_HOST.*/DATABASE_HOST=postgres/' /opt/bundler-node/.env
+sed -i 's/^DATABASE_USER.*/DATABASE_USER=bundlr/' /opt/bundler-node/.env
+sed -i 's/^DATABASE_PASSWORD.*/DATABASE_PASSWORD=changeme/' /opt/bundler-node/.env
+sed -i 's/^DATABASE_NAME.*/DATABASE_NAME=bundlr/' /opt/bundler-node/.env
+sed -i 's/^REDIS_HOST.*/REDIS_HOST=redis/' /opt/bundler-node/.env
+sed -i 's/^REDIS_PORT.*/REDIS_PORT=6379/' /opt/bundler-node/.env
+
+yarn
+chmod +x scripts/reset_all.sh
+scripts/reset_all.sh
+
+yarn prod:build
+yarn prod:start
